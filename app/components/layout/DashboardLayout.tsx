ac25e562d18899dd598d4d33fff6e757c9d768b4
bmnazmussakib <nazmussakib161@gmail.com>
2025-12-02 00:53:03 +0600

    homepage

--- app/components/layout/DashboardLayout.tsx
+++ app/components/layout/DashboardLayout.tsx
@@ -47,7 +47,7 @@ function DashboardLayout({ children }: Props) {
                                         </svg>
 
                                     </div>
-                                    <ul tabIndex="-1" className="dropdown-content menu bg-white rounded-box z-1 w-52 p-2 shadow-sm">
+                                    <ul tabIndex={-1} className="dropdown-content menu bg-white rounded-box z-1 w-52 p-2 shadow-sm">
                                         <li><a>Item 1</a></li>
                                         <li><a>Item 2</a></li>
                                     </ul>
@@ -62,7 +62,7 @@ function DashboardLayout({ children }: Props) {
 
 
                                     </div>
-                                    <ul tabIndex="-1" className="dropdown-content menu bg-white rounded-box z-1 w-52 p-2 shadow-sm">
+                                    <ul tabIndex={-1} className="dropdown-content menu bg-white rounded-box z-1 w-52 p-2 shadow-sm">
                                         <li><a>Item 1</a></li>
                                         <li><a>Item 2</a></li>
                                     </ul>
@@ -75,7 +75,7 @@ function DashboardLayout({ children }: Props) {
                                             </div>
                                         </div>
                                     </div>
-                                    <ul tabIndex="-1" className="dropdown-content menu bg-white rounded-box z-1 w-52 p-2 shadow-sm">
+                                    <ul tabIndex={-1} className="dropdown-content menu bg-white rounded-box z-1 w-52 p-2 shadow-sm">
                                         <li><a>Item 1</a></li>
                                         <li><a>Item 2</a></li>
                                     </ul>
