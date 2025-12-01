e00e13baafde47108b47b9a1ded7d3b2ba145b8b
bmnazmussakib <nazmussakib161@gmail.com>
2025-12-02 01:00:50 +0600

    homepage

--- app/components/layout/DashboardLayout.tsx
+++ app/components/layout/DashboardLayout.tsx
@@ -20,7 +20,7 @@ function DashboardLayout({ children }: Props) {
                 <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-content">
                     {/* Navbar */}
-                    <nav className="navbar w-full bg-stone-50 p-5">
+                    <nav className="navbar w-full h-20 bg-stone-50 px-5 border-b border-stone-200">
                         <label htmlFor="my-drawer-4" aria-label="open sidebar" className="text-3xl mr-3">
                             {/* Sidebar toggle icon */}
                             {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4"><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path><path d="M9 4v16"></path><path d="M14 10l2 2l-2 2"></path></svg> */}
@@ -84,13 +84,13 @@ function DashboardLayout({ children }: Props) {
                         </div>
                     </nav>
                     {/* Page content here */}
-                    <div className="p-4">{children}</div>
+                    <div className="p-10">{children}</div>
                 </div>
 
-                <div className="drawer-side is-drawer-close:overflow-visible">
+                <div className="drawer-side is-drawer-close:overflow-visible border-r border-stone-200">
                     <label htmlFor="my-drawer-4" aria-label="close sidebar" className="drawer-overlay"></label>
                     <div className="flex min-h-full flex-col items-start bg-stone-50 is-drawer-close:w-14 is-drawer-open:w-64">
-                        <div className='p-5 border-b border-stone-200 w-full flex items-center gap-2'>
+                        <div className='px-5 h-20 border-b border-stone-200 w-full flex items-center gap-2'>
                             <div className="text-[#343C6A]">
                                 <img src={sidebarData.logo.img} alt="" />
                             </div>
